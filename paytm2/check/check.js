ba5a1222811a9fe350382a7fdd98047f5a1b1fab
yogyasinghal <yogyasinghal21@gmail.com>
2021-01-16 01:31:22 +0530

    15

---
+++ paytm2/check/check.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+// const checkAuth  = require('../middleware/check-auth');
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.send("from index.js");
+  // res.render('index', { title: 'Express' });
+});
+
+router.post('/',(req,res)=>{
+  console.log("req",req);
+  console.log("hello from post");
+  res.send("yo");
+});
+
+module.exports = router;
--- routes/payment.js
+++ routes/payment.js
@@ -1,47 +1,57 @@
 var express = require('express');
 var router = express.Router();
 const port = process.env.PORT || 9000;
-const checksum_lib = require('../paytm/checksum/checksum');
-
+// const checksum_lib = require('../paytm/checksum/checksum');
+const paytm2 = require('../paytm2/check/check');
 // var data;
+console.log("paytm2",paytm2);
 console.log("in payment.js yo");
-router.get('/', function(req, res) {
-        // var sum = req.body.details.sum.toString();
-    // var id = req.body.details.userId.toString();
-    // data = req.body.details;
-    // var sum = 100;
-    // console.log("req = ",req.body);
-        let params = {}
-        params['MID'] 					= 'ukpqpp34255639627842';
-        params['WEBSITE']				= 'WEBSTAGING';
-        params['CHANNEL_ID']			= 'WEB';
-        params['INDUSTRY_TYPE_ID']	= 'Retail';
-        params['ORDER_ID']			= 'ORD09757656746';
-        params['CUST_ID'] 			= "id";
-        params['TXN_AMOUNT']		= "100";
-        params['CALLBACK_URL']		= 'https://yogyanewapi.herokuapp.com/success';
-        params['EMAIL']				= 'yogyatest5@gmail.com';
-        params['MOBILE_NO']			= '';
-
-        checksum_lib.genchecksum(params,'DVxaoJ6olr@AbADR', function (err, checksum) {
-
-            var txn_url = "https://securegw-stage.paytm.in/theia/processTransaction"; // for staging
-            // var txn_url = "https://securegw.paytm.in/theia/processTransaction"; // for production
-            
-            var form_fields = "";
-            for(var x in params){
-                form_fields += "<input type='hidden' name='"+x+"' value='"+params[x]+"' >";
-            }
-            form_fields += "<input type='hidden' name='CHECKSUMHASH' value='"+checksum+"' >";
-
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.write('<html><head><title>Merchant Checkout Page</title></head><body><center><h1>Please do not refresh this page...</h1></center><form method="post" action="'+txn_url+'" name="f1">'+form_fields+'</form><script type="text/javascript">document.f1.submit();</script></body></html>');
-            // res.send("res send");
-            res.end();
-    })
-//   res.send("from payment.js");
+router.get('/', function(req, res, next) {
+  res.send("from paymemt.js");
   // res.render('index', { title: 'Express' });
 });
+
+// router.get('/', function(req, res) {
+//         // var sum = req.body.details.sum.toString();
+//     // var id = req.body.details.userId.toString();
+//     // data = req.body.details;
+//     // var sum = 100;
+//     // console.log("req = ",req.body);
+//         let params = {}
+//         params['MID'] 					= 'ukpqpp34255639627842';
+//         params['WEBSITE']				= 'WEBSTAGING';
+//         params['CHANNEL_ID']			= 'WEB';
+//         params['INDUSTRY_TYPE_ID']	= 'Retail';
+//         params['ORDER_ID']			= 'ORD09757656746';
+//         params['CUST_ID'] 			= "id";
+//         params['TXN_AMOUNT']		= "100";
+//         params['CALLBACK_URL']		= 'https://yogyanewapi.herokuapp.com/success';
+//         params['EMAIL']				= 'yogyatest5@gmail.com';
+//         params['MOBILE_NO']			= '';
+
+//         checksum_lib.genchecksum(params,'DVxaoJ6olr@AbADR', function (err, checksum) {
+
+//             var txn_url = "https://securegw-stage.paytm.in/theia/processTransaction"; // for staging
+//             // var txn_url = "https://securegw.paytm.in/theia/processTransaction"; // for production
+            
+//             var form_fields = "";
+//             for(var x in params){
+//                 form_fields += "<input type='hidden' name='"+x+"' value='"+params[x]+"' >";
+//             }
+//             form_fields += "<input type='hidden' name='CHECKSUMHASH' value='"+checksum+"' >";
+
+//             res.writeHead(200, {'Content-Type': 'text/html'});
+//             res.write('<html><head><title>Merchant Checkout Page</title></head><body><center><h1>Please do not refresh this page...</h1></center><form method="post" action="'+txn_url+'" name="f1">'+form_fields+'</form><script type="text/javascript">document.f1.submit();</script></body></html>');
+//             // res.send("res send");
+//             res.end();
+//     })
+// //   res.send("from payment.js");
+//   // res.render('index', { title: 'Express' });
+// });
+
+
+
+
 // router.post('/',(req,res,next)=>{
 //     var sum = req.body.details.sum.toString();
 //     var id = req.body.details.userId.toString();
